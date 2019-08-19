f4331fe37f24d2cc1908d392cbe297aaa4b7237a
Larissa <dejamepienso@gmail.com>
2019-08-19 17:40:27 -0500

    Test some functions:

--- spec/IronhackerSpec.js
+++ spec/IronhackerSpec.js
@@ -4,5 +4,33 @@ describe('my fkn first callback', ()=>{
 		it('el array se debe llamar ironhackers', ()=>{
 			expect(Array.isArray(ironhackers)).toBeTruthy()
 		})
+
+		it('el array debería tener al menos 5 elementos', ()=>{
+			expect(ironhackers.length).toBeGreaterThanOrEqual(5)
+		})
+	
+		it('cada elemento debe ser un Objetote', ()=>{
+			ironhackers.map(ironhacker =>{
+				expect(typeof ironhacker).toBe('object')
+			})
+		})
+	
+		it('cada elemento debe tener "name", "lastname" & "age"', ()=>{
+			ironhackers.map(ironhacker =>{
+				expect(Object.keys(ironhacker)).toEqual(['name', 'lastname', 'age'])
+			})
+		})
 	})
+//next test
+	describe('Crea el método más fácil que quieras, cos you are already death', ()=>{
+		it('La funcion se debe llamar SayHello', ()=>{
+			expect(typeof sayHello).toBe('function')
+		})
+		
+		it('debe regresar "Hola <LARA>"', ()=>{
+			expect(sayHello('Lara')).toEqual('Hola Lara')
+			//expect(sayHello('Sam')).toEqual('Hola Sam')
+		})
+	})
+
 })
---
+++ src/Ironhacker.js
@@ -0,0 +1,29 @@
+let ironhackers = [
+	{
+		name: 'Juan',
+		lastname: 'Doe',
+		age:'25'
+	},{
+		name: 'César',
+		lastname: 'Doe',
+		age:'18'
+	},{
+		name: 'Lara',
+		lastname: 'Doe',
+		age:'28'
+	},{
+		name: 'Karina',
+		lastname: 'Doe',
+		age:'17'
+	},{
+		name: 'Diego',
+		lastname: 'Doe',
+		age:undefined
+	}
+]
+
+const sayHello = name => `Hola ${name}`
+
+// function sayHello(name) {
+// 	return `Hola me llamo${name}`
+// }
